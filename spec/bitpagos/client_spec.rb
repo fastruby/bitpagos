b674a209c337e6f1fd3b5de1ef3b571932cbb5c2
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-12-24 12:16:17 -0300

    Added examples for Bitpagos client init.

---
+++ spec/bitpagos/client_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe Bitpagos::Client do
+  describe "#new" do
+    subject { described_class.new(api_key) }
+
+    context "API key provided is blank" do
+      let(:api_key) { "" }
+      it "raises an InvalidApiKey error" do
+        expect { subject }.to raise_exception(Bitpagos::Errors::InvalidApiKey)
+      end
+    end
+
+    context "API key provided is nil" do
+      let(:api_key) { nil }
+      it "raises an InvalidApiKey error" do
+        expect { subject }.to raise_exception(Bitpagos::Errors::InvalidApiKey)
+      end
+    end
+
+    context "no API key is provided" do
+      subject { described_class.new }
+      it "raises an ArgumentError error" do
+        expect { subject }.to raise_exception(ArgumentError)
+      end
+    end
+
+    context "valid API key is provided" do
+      let(:api_key) { "ABCDEFGHIJK1234567890" }
+
+      it "doesn't raise any errors" do
+        expect { subject }.not_to raise_exception
+      end
+    end
+  end
+end
